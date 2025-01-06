40d035bb83abb2cbfe3e7105b6f13f16e227b458
pdm21 <pandsmar@gmail.com>
2025-01-06 11:58:57 -0500

    re-structured with utils, langgraph.json, requirements

--- agent-memory/agent.py
+++ agent-memory/agent.py
@@ -1,67 +1,30 @@
-from dotenv import load_dotenv
-load_dotenv()
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langchain_openai import ChatOpenAI
+from typing import TypedDict, Literal
+from langgraph.graph import StateGraph, END
+from agent.utils.nodes import call_model, should_continue, tool_node
+from agent.utils.state import AgentState
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # 
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
+# Define the config
+class GraphConfig(TypedDict):
+    # must be openai (have not added support for other models)
+    model_name: Literal["openai"]
 
-workflow = StateGraph(State)
-llm = ChatOpenAI(temperature=0, streaming=True)
+# Define a new graph
+workflow = StateGraph(AgentState, config_schema=GraphConfig)
 
-# Agent 1: chatbot 
-def chatbot(state: State):
-    response = llm.invoke(state["messages"])
-    return messages
-    # return {"messages": [llm.invoke(state["messages"])]}
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("action", tool_node)
 
-# Agent 2: 
-# take content from Agent 1, summarize it and 
-# identify key points (for potential DB store)
-def summarize_content(state: State):
-    print()
+workflow.set_entry_point("agent")
 
-# Agent 3: fetch current DB info
-# Agent 4: compare cur_DB with potential_new content
+workflow.add_conditional_edges(
+    "agent",
+    should_continue, 
+    {
+        "continue": "action",
+        "end": END,
+    },
+)
+workflow.add_edge("action", "agent")
 
-# need to provide DB access for chatbot agent (for during conversation)
-
-# Defining the graph
-workflow.set_entry_point("chatbot")
-workflow.add_node("chatbot", chatbot)
-workflow.add_edge("chatbot", END)
 graph = workflow.compile()
-
-conversation_history = {}
-id = 0
-
-def stream_graph_updates(user_input: str):
-    # id += 1
-
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-            maintain_memory(conversation_history, id, value["messages"][-1].content, user_input)
-
-
-def maintain_memory(conversation_history, id, AIMessage, HumanMessage):
-    conversation_history[id] = [f"AIMessage: {AIMessage}" + f"HumanMessage: {HumanMessage}"]
-
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
