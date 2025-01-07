df0edef56e1d8e71d76bba49f608b334d97d0b55
pdm21 <pandsmar@gmail.com>
2025-01-06 22:57:51 -0500

    nodes.py: model config

--- agent-memory/agent/utils/nodes.py
+++ agent-memory/agent/utils/nodes.py
@@ -33,7 +33,7 @@ system_prompt = """You are a helpful assistant. You answer whatever questions I 
 def call_model(state, config):
     messages = state["messages"]
     messages = [{"role": "system", "content": system_prompt}] + messages
-    model_name = config.get('configurable', {}).get("model_name", "anthropic")
+    model_name = config.get('configurable', {}).get("model_name", "openai")
     model = _get_model(model_name)
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
